5d0dc13daaaf6935b4e28aae463a03e8ae17ccbb
Matthew Wright <mattwright324@gmail.com>
2017-12-30 08:50:09 -0500

    Modified page. Added scroll to top button. Fixed-to-top navbar.

--- js/pdnpi.js
+++ js/pdnpi.js
@@ -198,3 +198,18 @@ window.onload = function() {
 		console.log(err);
 	});
 }
+
+// Scroll to top
+$(document).ready(function(){ 
+    $(window).scroll(function(){ 
+        if ($(this).scrollTop() > 100) { 
+            $('#scroll').fadeIn(); 
+        } else { 
+            $('#scroll').fadeOut(); 
+        } 
+    }); 
+    $('#scroll').click(function(){ 
+        $("html, body").animate({ scrollTop: 0 }, 600); 
+        return false; 
+    }); 
+});
